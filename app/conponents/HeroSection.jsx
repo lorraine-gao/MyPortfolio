dd9f27326f66538e39515686e27aad9288b0945f
leyan gao <leyan gao>
2025-03-18 17:58:13 +1100

    initial code

---
+++ app/conponents/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>HeroSection
+      <div>hihihi</div>
+      <div>www</div>
+    </div>
+
+  )
+}
+
+export default HeroSection
